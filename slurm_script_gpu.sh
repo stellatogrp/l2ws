ac8691b759eca44efa3877c5b254b0fcaa3b0131
Rajiv Sambharya <rajivs@della8.princeton.edu>
2023-03-17 17:06:54 -0400

    tried robust ls experiments cluster

--- slurm_script_gpu.sh
+++ slurm_script_gpu.sh
@@ -5,9 +5,9 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=20G         # memory per cpu-core (4G is default)
+#SBATCH --mem-per-cpu=40G         # memory per cpu-core (4G is default)
 #SBATCH --array=0             # job array with index values 0, 1, 2, 3, 4
-#SBATCH --time=02:59:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=00:59:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=all          # send email on job start, end and fault
 #SBATCH --mail-user=rajivs@princeton.edu # 
 #SBATCH --gres=gpu:1 
@@ -17,7 +17,7 @@ echo "My SLURM_ARRAY_TASK_ID is $SLURM_ARRAY_TASK_ID"
 echo "Executing on the machine:" $(hostname)
 
 
-python l2ws_train_script.py robust_kalman cluster
+python l2ws_train_script.py robust_ls cluster
 # python aggregate_slurm_runs_script.py robust_ls cluster
 
 
