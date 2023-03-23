b1b893f87f550fb06bd7b91aa505816e6ba0a296
Rajiv Sambharya <rajivs@della8.princeton.edu>
2023-03-23 17:32:34 -0400

    robust ls experiments for 30,60 from the weekend

--- slurm_script_cpu.sh
+++ slurm_script_cpu.sh
@@ -5,9 +5,9 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=150G         # memory per cpu-core (4G is default)
+#SBATCH --mem-per-cpu=50G         # memory per cpu-core (4G is default)
 #SBATCH --array=0             # job array with index values 0, 1, 2, 3, 4
-#SBATCH --time=15:56:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=0:59:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=all          # send email on job start, end and fault
 #SBATCH --mail-user=rajivs@princeton.edu # 
 
