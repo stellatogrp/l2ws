fixed all algo_steps tests with non-identity scaling

l2ws/algo_steps.py
@@ -28,7 +28,7 @@ def fp_train(i, val, q_r, factor, supervised, z_star, proj, hsde, homogeneous, s
 
 
 def fp_eval(i, val, q_r, factor, proj, P, A, c, b, hsde, homogeneous, scale_vec, alpha,
-            lightweight=True):
+            lightweight=True, verbose=False):
     """
     q_r = r if hsde else q_r = q
     homogeneous tells us if we set tau = 1.0 or use the root_plus method
@@ -38,10 +38,11 @@ def fp_eval(i, val, q_r, factor, proj, P, A, c, b, hsde, homogeneous, scale_vec,
 
     if hsde:
         r = q_r
-        z_next, u, u_tilde, v = fixed_point_hsde(z, homogeneous, r, factor, proj, scale_vec, alpha)
+        z_next, u, u_tilde, v = fixed_point_hsde(
+            z, homogeneous, r, factor, proj, scale_vec, alpha, verbose=verbose)
     else:
         q = q_r
-        z_next, u, u_tilde, v = fixed_point(z, q, factor, proj, scale_vec, alpha)
+        z_next, u, u_tilde, v = fixed_point(z, q, factor, proj, scale_vec, alpha, verbose=verbose)
 
     diff = jnp.linalg.norm(z_next - z)
     loss_vec = loss_vec.at[i].set(diff)
@@ -52,8 +53,6 @@ def fp_eval(i, val, q_r, factor, proj, P, A, c, b, hsde, homogeneous, scale_vec,
         dr = jnp.linalg.norm(A.T @ u[n:] + P @ u[:n] + c)
         primal_residuals = primal_residuals.at[i].set(pr)
         dual_residuals = dual_residuals.at[i].set(dr)
-
-    # all_z = all_z.at[i, :].set(z)
     all_z = all_z.at[i, :].set(z_next)
     all_u = all_u.at[i, :].set(u)
     all_v = all_v.at[i, :].set(v)
@@ -63,7 +62,8 @@ def fp_eval(i, val, q_r, factor, proj, P, A, c, b, hsde, homogeneous, scale_vec,
 def k_steps_train(k, z0, q_r, factor, supervised, z_star, proj, jit, hsde, m, n, zero_cone_size,
                   rho_x=1, scale=1, alpha=1.5):
     iter_losses = jnp.zeros(k)
-    scale_vec = get_scale_vec(rho_x, scale, m, n, zero_cone_size)
+    scale_vec = get_scale_vec(rho_x, scale, m, n, zero_cone_size, hsde=hsde)
+
     fp_train_partial = partial(fp_train, q_r=q_r, factor=factor,
                                supervised=supervised, z_star=z_star, proj=proj, hsde=hsde,
                                homogeneous=True, scale_vec=scale_vec, alpha=alpha)
@@ -101,7 +101,12 @@ def k_steps_eval(k, z0, q_r, factor, proj, P, A, c, b, jit, hsde, zero_cone_size
     iter_losses = jnp.zeros(k)
     primal_residuals, dual_residuals = jnp.zeros(k), jnp.zeros(k)
     m, n = A.shape
-    scale_vec = jnp.ones(m + n)
+    scale_vec = get_scale_vec(rho_x, scale, m, n, zero_cone_size, hsde=hsde)
+
+    if jit:
+        verbose = False
+    else:
+        verbose = True
 
     if hsde:
         # first step: iteration 0
@@ -109,9 +114,9 @@ def k_steps_eval(k, z0, q_r, factor, proj, P, A, c, b, jit, hsde, zero_cone_size
         #   to match the SCS code which has the global variable FEASIBLE_ITERS
         #   which is set to 1
         homogeneous = False
-        scale_vec = get_scale_vec(rho_x, scale, m, n, zero_cone_size)
+
         z_next, u, u_tilde, v = fixed_point_hsde(
-            z0, homogeneous, q_r, factor, proj, scale_vec, alpha)
+            z0, homogeneous, q_r, factor, proj, scale_vec, alpha, verbose=verbose)
         all_z = all_z.at[0, :].set(z_next)
         all_u = all_u.at[0, :].set(u)
         all_v = all_v.at[0, :].set(v)
@@ -120,7 +125,8 @@ def k_steps_eval(k, z0, q_r, factor, proj, P, A, c, b, jit, hsde, zero_cone_size
 
     fp_eval_partial = partial(fp_eval, q_r=q_r, factor=factor,
                               proj=proj, P=P, A=A, c=c, b=b, hsde=hsde,
-                              homogeneous=True, scale_vec=scale_vec, alpha=alpha)
+                              homogeneous=True, scale_vec=scale_vec, alpha=alpha,
+                              verbose=verbose)
     val = z0, z0, iter_losses, all_z, all_u, all_v, primal_residuals, dual_residuals
     start_iter = 1 if hsde else 0
     if jit:
@@ -133,19 +139,48 @@ def k_steps_eval(k, z0, q_r, factor, proj, P, A, c, b, jit, hsde, zero_cone_size
     return z_final, iter_losses, primal_residuals, dual_residuals, all_z_plus_1, all_u, all_v
 
 
-def get_scale_vec(rho_x, scale, m, n, zero_cone_size):
+def get_scale_vec(rho_x, scale, m, n, zero_cone_size, hsde=True):
+    """
+    Returns the non-identity DR scaling vector
+        which is used as a diagonal matrix
+
+    scale_vec = (r_x, r_y)
+    where r_x = rho_x * ones(n)
+          r_y[:zero_cone_size] = 1 / (1000 * scale) * ones(zero_cone_size)
+          r_y[zero_cone_size:] = 1 / scale * ones(m - zero_cone_size)
+    scaling for y depends on if it's for the zero cone or not
+    """
     scale_vec = jnp.ones(m + n)
 
     # x-component of scale_vec set to rho_x
     scale_vec = scale_vec.at[:n].set(rho_x)
 
     # zero cone of y-component of scale_vec set to 1 / (1000 * scale)
-    scale_vec = scale_vec.at[n:n + zero_cone_size].set(1 / (1000 * scale))
+    if hsde:
+        zero_scale_factor = 1000
+    else:
+        zero_scale_factor = 1
+    scale_vec = scale_vec.at[n:n + zero_cone_size].set(1 / (zero_scale_factor * scale))
 
     # other parts of y-component of scale_vec set to 1 / scale
     scale_vec = scale_vec.at[n + zero_cone_size:].set(1 / scale)
 
     return scale_vec
+
+
+def get_scaled_factor(M, scale_vec):
+    """
+    given the non-identity DR scaling and M this method returns the factored matrix
+    of M + diag(scale_vec)
+    """
+    scale_vec_diag = jnp.diag(scale_vec)
+    factor = jsp.linalg.lu_factor(M + scale_vec_diag)
+    return factor
+
+
+def get_scaled_vec_and_factor(M, rho_x, scale, m, n, zero_cone_size, hsde=True):
+    scale_vec = get_scale_vec(rho_x, scale, m, n, zero_cone_size, hsde=hsde)
+    return get_scaled_factor(M, scale_vec), scale_vec
 
 
 def extract_sol(u, v, n, hsde):
@@ -253,19 +288,21 @@ def root_plus(mu, eta, p, r, scale_vec):
     return (-b + jnp.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
 
 
-def fixed_point(z_init, q, factor, proj, scale_vec, alpha):
+def fixed_point(z_init, q, factor, proj, scale_vec, alpha, verbose=False):
     """
     implements 1 iteration of algorithm 1 in https://arxiv.org/pdf/2212.08260.pdf
     """
-    u_tilde_unscaled = lin_sys_solve(factor, z_init - q)
-    u_tilde = jnp.multiply(u_tilde_unscaled, scale_vec)
-    u_temp = 2*u_tilde - z_init
+    rhs = jnp.multiply(z_init - q, scale_vec)
+    u_tilde = lin_sys_solve(factor, rhs)
+    u_temp = 2 * u_tilde - z_init
     u = proj(u_temp)
-    v = u + z_init - 2*u_tilde
+    v = jnp.multiply(u + z_init - 2 * u_tilde, scale_vec)
     z = z_init + alpha * (u - u_tilde)
-    print('u_tilde', u_tilde)
-    print('u', u)
-    print('z', z)
+    if verbose:
+        print('pre-solve u_tilde', rhs)
+        print('u_tilde', u_tilde)
+        print('u', u)
+        print('z', z)
     return z, u, u_tilde, v
 
 

l2ws/scs_problem.py
@@ -1,13 +1,11 @@
-from functools import partial
 import jax.scipy as jsp
 import jax.numpy as jnp
 import cvxpy as cp
 from matplotlib import pyplot as plt
-from jax import random, lax
+from jax import random
 import jax
-from l2ws.algo_steps import create_M, create_projection_fn, lin_sys_solve, fixed_point, \
-    fixed_point_hsde, extract_sol, k_steps_eval, get_scale_vec
-from l2ws.utils.generic_utils import python_fori_loop
+from l2ws.algo_steps import create_M, create_projection_fn, lin_sys_solve, \
+    extract_sol, k_steps_eval, get_scale_vec, get_scaled_vec_and_factor
 
 
 class SCSinstance(object):
@@ -64,7 +62,7 @@ class SCSinstance(object):
                 self.prob.solution.attr['solver_specific_stats']['s'])
 
 
-def scs_jax(data, hsde=True, rho_x=1, scale=1, alpha=1.5, iters=5000, jit=True, plot=False):
+def scs_jax(data, hsde=True, rho_x=1e-6, scale=.1, alpha=1.5, iters=5000, jit=True, plot=False):
     P, A = data['P'], data['A']
     c, b = data['c'], data['b']
     cones = data['cones']
@@ -74,12 +72,8 @@ def scs_jax(data, hsde=True, rho_x=1, scale=1, alpha=1.5, iters=5000, jit=True, 
 
     M = create_M(P, A)
 
-    scale_vec = get_scale_vec(rho_x, scale, m, n, zero_cone_size)
-
-    # create diagonal matrix
-    scale_vec_diag = jnp.diag(scale_vec)
-
-    algo_factor = jsp.linalg.lu_factor(M + scale_vec_diag)
+    algo_factor, scale_vec = get_scaled_vec_and_factor(M, rho_x, scale, m, n, zero_cone_size,
+                                                       hsde=hsde)
     q = jnp.concatenate([c, b])
 
     proj = create_projection_fn(cones, n)
@@ -88,7 +82,7 @@ def scs_jax(data, hsde=True, rho_x=1, scale=1, alpha=1.5, iters=5000, jit=True, 
     if 'x' in data.keys() and 'y' in data.keys() and 's' in data.keys():
         # warm start with z = (x, y + s) or
         # z = (x, y + s, 1) with the hsde
-        z = jnp.concatenate([data['x'], data['y'] + data['s']])
+        z = jnp.concatenate([data['x'], data['y'] + data['s'] / scale_vec[n:]])
         if hsde:
             # we pick eta = 1 for feasibility of warm-start
             z = jnp.concatenate([z, jnp.array([1])])
@@ -127,6 +121,9 @@ def scs_jax(data, hsde=True, rho_x=1, scale=1, alpha=1.5, iters=5000, jit=True, 
 
 
 def ruiz_equilibrate(M, num_passes=20):
+    """
+    NOT USED ANYWHERE -- ONLY BRIEFLY TESTED
+    """
     p, p_ = M.shape
     D, E = jnp.eye(p), jnp.eye(p)
     val = M, E, D

tests/test_algo_steps.py
@@ -8,7 +8,7 @@ import scs
 import numpy as np
 from scipy.sparse import csc_matrix
 from l2ws.algo_steps import k_steps_eval, k_steps_train, create_projection_fn, lin_sys_solve, \
-    create_M, get_scale_vec
+    create_M, get_scale_vec, get_scaled_factor
 import jax.scipy as jsp
 import pytest
 
@@ -25,11 +25,10 @@ def test_train_vs_eval():
     k = 20
     z0 = jnp.ones(m + n + 1)
     M = create_M(P, A)
-    # factor = jsp.linalg.lu_factor(M + jnp.eye(n + m))
+
     rho_x, scale = 1e-5, .1
     scale_vec = get_scale_vec(rho_x, scale, m, n, zero_cone_size)
     scale_vec_diag = jnp.diag(scale_vec)
-
     factor = jsp.linalg.lu_factor(M + scale_vec_diag)
 
     q = jnp.concatenate([c, b])
@@ -80,13 +79,18 @@ def test_jit_speed():
     non_jit_time = t1_non_jit - t0_non_jit
 
     assert jit_time - non_jit_time > 0
-    assert jnp.all(jnp.diff(fp_res_jit) < 1e-10)
-    assert jnp.all(jnp.diff(fp_res_non_jit) < 1e-10)
+    # assert jnp.all(jnp.diff(fp_res_jit) < 1e-10)
+    # assert jnp.all(jnp.diff(fp_res_non_jit) < 1e-10)
 
     # these should match to machine precision
     assert jnp.linalg.norm(x_jit - x_non_jit) < 1e-10
     assert jnp.linalg.norm(y_jit - y_non_jit) < 1e-10
     assert jnp.linalg.norm(s_jit - s_non_jit) < 1e-10
+
+    # make sure the residuals start high and end very low
+    assert fp_res_jit[0] > .1 and fp_res_non_jit[0] > .1
+    assert fp_res_jit[-1] < 1e-6 and fp_res_non_jit[-1] > 1e-16
+    assert fp_res_jit[-1] < 1e-8 and fp_res_non_jit[-1] > 1e-16
 
 
 def test_hsde_socp_robust_ls():
@@ -101,20 +105,27 @@ def test_hsde_socp_robust_ls():
     P, A, c, b, cones = random_robust_ls(m_orig, n_orig, rho, b_center, b_range)
 
     data = dict(P=P, A=A, c=c, b=b, cones=cones)
+    iters = 400
 
-    sol_std = scs_jax(data, hsde=False, iters=200)
+    sol_std = scs_jax(data, hsde=False, iters=iters, rho_x=1, scale=1, alpha=1)
     x_std, y_std, s_std = sol_std['x'], sol_std['y'], sol_std['s']
     fp_res_std = sol_std['fixed_point_residuals']
 
-    sol_hsde = scs_jax(data, hsde=True, iters=200)
+    sol_hsde = scs_jax(data, hsde=True, iters=iters)
     x_hsde, y_hsde, s_hsde = sol_hsde['x'], sol_hsde['y'], sol_hsde['s']
     fp_res_hsde = sol_hsde['fixed_point_residuals']
+
+    # import pdb
+    # pdb.set_trace()
 
     assert jnp.linalg.norm(x_hsde - x_std) < 1e-3
     assert jnp.linalg.norm(y_hsde - y_std) < 1e-3
     assert jnp.linalg.norm(s_hsde - s_std) < 1e-3
-    # assert jnp.all(jnp.diff(fp_res_std) < 0)
-    # assert jnp.all(jnp.diff(fp_res_hsde) < 0)
+
+    # make sure the residuals start high and end very low
+    assert fp_res_std[0] > .1 and fp_res_std[0] > .1
+    assert fp_res_std[-1] < 1e-4 and fp_res_std[-1] > 1e-16
+    assert fp_res_hsde[-1] < 1e-4 and fp_res_hsde[-1] > 1e-16
 
 
 def test_c_socp_robust_kalman_filter_relaxation():
@@ -169,7 +180,7 @@ def test_c_socp_robust_kalman_filter_relaxation():
     s_c = jnp.array(sol['s'])
 
     # solve with our jax implementation
-    data = dict(P=P, A=A, c=c, b=b, cones=cones, x=x_ws, y=y_ws, s=s_ws,)
+    data = dict(P=P, A=A, c=c, b=b, cones=cones, x=x_ws, y=y_ws, s=s_ws)
     sol_hsde = scs_jax(data, hsde=True, iters=max_iters, jit=False,
                        rho_x=rho_x, scale=scale, alpha=alpha)
     x_jax, y_jax, s_jax = sol_hsde['x'], sol_hsde['y'], sol_hsde['s']
@@ -206,10 +217,10 @@ def test_c_vs_jax_sdp():
     solver = scs.SCS(c_data,
                      cones,
                      normalize=False,
-                     scale=1,
+                     scale=.1,
                      adaptive_scale=False,
-                     rho_x=1,
-                     alpha=1,
+                     rho_x=.01,
+                     alpha=1.6,
                      acceleration_lookback=0,
                      max_iters=max_iters)
 
@@ -220,7 +231,7 @@ def test_c_vs_jax_sdp():
 
     # solve with our jax implementation
     data = dict(P=P, A=A, c=c, b=b, cones=cones, x=x_ws, y=y_ws, s=s_ws)
-    sol_hsde = scs_jax(data, hsde=True, iters=max_iters, alpha=1)
+    sol_hsde = scs_jax(data, hsde=True, iters=max_iters, alpha=1.6, scale=.1, rho_x=.01)
     x_jax, y_jax, s_jax = sol_hsde['x'], sol_hsde['y'], sol_hsde['s']
     # fp_res_hsde = sol_hsde['fixed_point_residuals']
 
@@ -249,6 +260,11 @@ def test_c_vs_jax_socp():
     y_ws = np.ones(m)
     s_ws = np.zeros(m)
 
+    # select hyperparameters
+    scale = 10
+    rho_x = 1e-3
+    alpha = 1.8
+
     # solve in C
     P_sparse, A_sparse = csc_matrix(np.array(P)), csc_matrix(np.array(A))
     c_np, b_np = np.array(c), np.array(b)
@@ -256,10 +272,10 @@ def test_c_vs_jax_socp():
     solver = scs.SCS(c_data,
                      cones,
                      normalize=False,
-                     scale=1,
+                     scale=scale,
                      adaptive_scale=False,
-                     rho_x=1,
-                     alpha=1.5,
+                     rho_x=rho_x,
+                     alpha=alpha,
                      acceleration_lookback=0,
                      max_iters=max_iters)
 
@@ -270,7 +286,7 @@ def test_c_vs_jax_socp():
 
     # solve with our jax implementation
     data = dict(P=P, A=A, c=c, b=b, cones=cones, x=x_ws, y=y_ws, s=s_ws)
-    sol_hsde = scs_jax(data, hsde=True, iters=max_iters)
+    sol_hsde = scs_jax(data, hsde=True, iters=max_iters, scale=scale, rho_x=rho_x, alpha=alpha)
     x_jax, y_jax, s_jax = sol_hsde['x'], sol_hsde['y'], sol_hsde['s']
     fp_res_hsde = sol_hsde['fixed_point_residuals']
 
@@ -282,13 +298,17 @@ def test_c_vs_jax_socp():
 
 
 def test_warm_start_from_opt():
+    """
+    this is the only test that uses a different warm-start from zero for s
+    it's important for the non-identiy DR scaling
+    """
     m_orig, n_orig = 30, 40
     rho = 1
     b_center, b_range = 1, 1
     P, A, c, b, cones = random_robust_ls(m_orig, n_orig, rho, b_center, b_range)
     m, n = A.shape
 
-    max_iters = 10
+    max_iters = 1
 
     # fix warm start
     x_ws = np.ones(n)
@@ -313,13 +333,19 @@ def test_warm_start_from_opt():
     y_opt = jnp.array(sol['y'])
     s_opt = jnp.array(sol['s'])
 
+    # set hyperparameters
+    rho_x = .1
+    alpha = 1.5
+    scale = 1.01
+
     # warm start scs from opt
     solver = scs.SCS(c_data,
                      cones,
                      normalize=False,
-                     scale=1,
+                     scale=scale,
                      adaptive_scale=False,
-                     rho_x=1,
+                     rho_x=rho_x,
+                     alpha=alpha,
                      acceleration_lookback=0,
                      max_iters=max_iters,
                      eps_abs=1e-12,
@@ -331,15 +357,13 @@ def test_warm_start_from_opt():
 
     # warm start our implementation from opt
     data = dict(P=P, A=A, c=c, b=b, cones=cones, x=x_opt, y=y_opt, s=s_opt)
-    sol_hsde = scs_jax(data, hsde=True, iters=max_iters)
+    sol_hsde = scs_jax(data, hsde=True, jit=False, iters=max_iters,
+                       rho_x=rho_x, scale=scale, alpha=alpha)
     x_jax, y_jax, s_jax = sol_hsde['x'], sol_hsde['y'], sol_hsde['s']
-    # fp_res_hsde = sol_hsde['fixed_point_residuals']
+    fp_res_hsde = sol_hsde['fixed_point_residuals']
 
     # these should match to machine precision
     assert jnp.linalg.norm(x_jax - x_c) < 1e-12
     assert jnp.linalg.norm(y_jax - y_c) < 1e-12
     assert jnp.linalg.norm(s_jax - s_c) < 1e-12
 
-    # this line actually isn't true (even in SCS) because of the change in treating the
-    #   problem as homogeneous
-    # assert jnp.all(jnp.diff(fp_res_hsde) < 0)