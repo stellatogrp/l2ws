43de515e573a2e8eb5b3a68525412a7d358e7da1
Rajiv Sambharya <Rajiv>
2023-05-03 10:46:11 -0400

    cleaned up some comments in the osqp model

--- l2ws/osqp_model.py
+++ l2ws/osqp_model.py
@@ -11,21 +11,13 @@ class OSQPmodel(L2WSmodel):
         super(OSQPmodel, self).__init__(input_dict)
 
     def initialize_algo(self, input_dict):
-        # self.A = input_dict['A']
         A = input_dict['A']
         factor = input_dict['factor']
         m, n = A.shape
-        # if self.static_flag:
-        #     self.static_M = input_dict['static_M']
-        #     self.static_algo_factor = input_dict['static_algo_factor']
-        # else:
-        #     pass
-
         self.q_mat_train, self.q_mat_test = input_dict['q_mat_train'], input_dict['q_mat_test']
         rho = input_dict.get('rho', 1)
         sigma = input_dict.get('sigma', 1)
 
-        # self.m, self.n = self.A.shape
         self.output_size = n + m
 
         self.k_steps_train_fn = partial(k_steps_train_osqp, factor=factor, A=A, rho=rho, sigma=sigma, jit=self.jit)
