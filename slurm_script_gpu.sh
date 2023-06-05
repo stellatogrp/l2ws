43378fc83b7f3d72a024ac6dee06d05fc08dc612
Rajiv Sambharya <rajivs@della8.princeton.edu>
2023-06-04 20:05:34 -0400

    trying things for phase retrieval on cluster

--- slurm_script_gpu.sh
+++ slurm_script_gpu.sh
@@ -5,9 +5,9 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=100G         # memory per cpu-core (4G is default)
+#SBATCH --mem-per-cpu=200G         # memory per cpu-core (4G is default)
 #SBATCH --array=0             # job array with index values 0, 1, 2, 3, 4
-#SBATCH --time=00:59:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=02:59:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=all          # send email on job start, end and fault
 #SBATCH --mail-user=rajivs@princeton.edu # 
 #SBATCH --gres=gpu:1 
